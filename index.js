4e6d941cfd12c273cb63b197492c82c713734203
taghikhah <m.taghikhah@gmail.com>
2022-03-06 11:18:50 +0100

    ec2

--- index.js
+++ index.js
@@ -14,18 +14,29 @@ require("./startup/config")();
 // wsServer;
 
 const { sendKafka } = require("./kafka");
-// sendKafka();
 
-var time = 1;
+// "fbdf30d8-e0a0-4d1d-8b91-9498ebc62871" - 6 MB
+// b569f132-8434-4f80-96c5-234d9f8b1069 - 30 MB
+// f1b4a809-266a-418c-aed9-9abcea60abf4 - 150 MB
+// bfb97921-c4f3-4b44-b51b-b6cff1665a1d - 300 MB
+// 37d2d8da-f197-4438-bdf6-e9f511005b36 - 500 MB
+const messages = [
+  "fbdf30d8-e0a0-4d1d-8b91-9498ebc62871",
+  "b569f132-8434-4f80-96c5-234d9f8b1069",
+];
 
-var interval = setInterval(function () {
-  if (time <= 20) {
-    sendKafka();
-    time++;
-  } else {
-    clearInterval(interval);
-  }
-}, 10000);
+for (const message of messages) {
+  sendKafka(message);
+}
+// var time = 1;
+// var interval = setInterval(function () {
+//   if (time <= 20) {
+//     sendKafka();
+//     time++;
+//   } else {
+//     clearInterval(interval);
+//   }
+// }, 20000);
 
 const consumers = require("./pipeline/consumers");
 // start the consumer
--- kafka.js
+++ kafka.js
@@ -3,16 +3,11 @@ const { getMetaData } = require("./pipeline/metadata");
 const mongoose = require("mongoose");
 const config = require("config");
 
-async function sendKafka() {
-  // "fbdf30d8-e0a0-4d1d-8b91-9498ebc62871" - 6 MB
-  // b569f132-8434-4f80-96c5-234d9f8b1069 - 30 MB
-  // f1b4a809-266a-418c-aed9-9abcea60abf4 - 150 MB
-  // bfb97921-c4f3-4b44-b51b-b6cff1665a1d - 300 MB
-  // 37d2d8da-f197-4438-bdf6-e9f511005b36 - 500 MB
+async function sendKafka(id) {
   try {
     await mongoose.connect(config.get("db"));
     const message = {
-      blFileIdentifer: "fbdf30d8-e0a0-4d1d-8b91-9498ebc62871",
+      blFileIdentifer: id,
     };
 
     // Save Messages to the Database
